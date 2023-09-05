f28009ddfd92fff882bbdc3efe1a30f25028ca12
imsubhajit98 <subhajit06521@gmail.com>
2023-09-05 12:46:17 +0530

    selection range change

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function App() {
 
   const copyPasswordToClipboard=useCallback(()=>{
     passwordRef.current?.select();
-    passwordRef.current?.setSelectionRange(0,3);
+    // passwordRef.current?.setSelectionRange(0,3);
     window.navigator.clipboard.writeText(password);
   },[password])
 
